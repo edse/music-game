c3287d12aca9425ec2962b60c2de0fcdd738d44f
Emerson Estrella <emerson.estrella@gmail.com>
2024-10-13 21:13:30 +0200

    feat: build 0.02

--- src/js/main.js
+++ src/js/main.js
@@ -11,49 +11,96 @@ var col2PressStart = 0;
 var col3PressStart = 0;
 var col4PressStart = 0;
 
+var video = document.getElementById("video")
+
 playVideo = function(){
-    document.getElementById("video").play()
+    video.play()
     document.getElementById("play-button").classList.add("hidden");
 }
 
-document.addEventListener("keydown", function(e) {
-    video = document.getElementById("video")
+col1StartBlock = function(){
+    document.getElementById("col-1").classList.add("col-1-selected");
+    col1PressStart = video.currentTime
+}
+
+col1EndBlock = function(){
+    document.getElementById("col-1").classList.remove("col-1-selected");
+    console.log(col1PressStart + " -> " + video.currentTime)
+}
+
+col2StartBlock = function(){
+    document.getElementById("col-2").classList.add("col-2-selected");
+    col2PressStart = video.currentTime
+}
+
+col2EndBlock = function(){
+    document.getElementById("col-2").classList.remove("col-2-selected");
+    console.log(col2PressStart + " -> " + video.currentTime)
+}
+
+col3StartBlock = function(){
+    document.getElementById("col-3").classList.add("col-3-selected");
+    col3PressStart = video.currentTime
+}
+
+col3EndBlock = function(){
+    document.getElementById("col-3").classList.remove("col-3-selected");
+    console.log(col3PressStart + " -> " + video.currentTime)
+}
+
+col4StartBlock = function(){
+    document.getElementById("col-4").classList.add("col-4-selected");
+    col4PressStart = video.currentTime
+}
+
+col4EndBlock = function(){
+    document.getElementById("col-4").classList.remove("col-4-selected");
+    console.log(col4PressStart + " -> " + video.currentTime)
+}
+
+startBlock = function(e){
     if(e.key === "a") {
-        document.getElementById("col-1").classList.add("col-1-selected");
-        col1PressStart = video.currentTime
+        col1StartBlock()
     }
     if(e.key === "s") {
-        document.getElementById("col-2").classList.add("col-2-selected");
-        col2PressStart = video.currentTime
+        col2StartBlock()
     }
     if(e.key === "d") {
-        document.getElementById("col-3").classList.add("col-3-selected");
-        col3PressStart = video.currentTime
+        col3StartBlock()
     }
     if(e.key === "f") {
-        document.getElementById("col-4").classList.add("col-4-selected");
-        col4PressStart = video.currentTime
+        col4StartBlock()
     }
-});
+}
 
-document.addEventListener("keyup", function(e) {
-    console.log(e.key)
+endBlock = function(e){
     if(e.key === "a") {
-        document.getElementById("col-1").classList.remove("col-1-selected");
-        console.log(col1PressStart + " -> " + video.currentTime)
+        col1EndBlock()
     }
     if(e.key === "s") {
-        document.getElementById("col-2").classList.remove("col-2-selected");
-        console.log(col2PressStart + " -> " + video.currentTime)
+        col2EndBlock()
     }
     if(e.key === "d") {
-        document.getElementById("col-3").classList.remove("col-3-selected");
-        console.log(col3PressStart + " -> " + video.currentTime)
+        col3EndBlock()
     }
     if(e.key === "f") {
-        document.getElementById("col-4").classList.remove("col-4-selected");
-        console.log(col4PressStart + " -> " + video.currentTime)
+        col4EndBlock()
     }
-});
+}
 
+// key press
+document.addEventListener("keydown", startBlock);
+document.addEventListener("keyup", endBlock);
+
+// touch
+document.getElementById("col-1").addEventListener("touchstart", col1StartBlock);
+document.getElementById("col-1").addEventListener("touchend", col1EndBlock);
+document.getElementById("col-2").addEventListener("touchstart", col2StartBlock);
+document.getElementById("col-2").addEventListener("touchend", col2EndBlock);
+document.getElementById("col-3").addEventListener("touchstart", col3StartBlock);
+document.getElementById("col-3").addEventListener("touchend", col3EndBlock);
+document.getElementById("col-4").addEventListener("touchstart", col4StartBlock);
+document.getElementById("col-4").addEventListener("touchend", col4EndBlock);
+
+// click
 document.getElementById("play-button").addEventListener("click", playVideo);
